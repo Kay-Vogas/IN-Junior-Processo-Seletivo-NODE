481544988c2f091e173b957fbd1a19ac68710077
Kay-Vogas <kay.vogas@gmail.com>
2026-07-24 09:45:02 -0300

    Configuração Projeto

---
+++ tsup.config.ts
@@ -0,0 +1,15 @@
+import {defineConfig} from 'tsup'
+
+export default defineConfig({
+    // Define o diretório raiz, que devem ser compilados
+    entry: ['src'],
+    // Define a saída dos arquivos ,ESModules
+    format: ['esm'],
+    // Diretório onde os arquivos javascript serão criados
+    outDir: 'build',
+    // Auxilia na hora de debugar o código
+    sourcemap: true,
+    shims: true, 
+    // Diz a versão alvo do javascript que iremos utilizar
+    target: 'esnext'
+});
