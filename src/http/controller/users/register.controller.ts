8856601988f217fb9bc404126ceb462e8b9ef949
Kay-Vogas <kay.vogas@gmail.com>
2026-07-28 14:46:48 -0300

    feat: verificação por meio de query para Email/Username

--- src/http/controller/users/register.controller.ts
+++ src/http/controller/users/register.controller.ts
@@ -1,6 +1,8 @@
 import {z} from 'zod'
 import { prisma } from '@/libs/prisma.js'
 import type { FastifyReply , FastifyRequest } from 'fastify'
+import { hash } from 'bcryptjs'
+import { env } from '@/env/index.js'
 
 export async function register(request: FastifyRequest,reply: FastifyReply){
 
@@ -14,12 +16,30 @@ export async function register(request: FastifyRequest,reply: FastifyReply){
 
     const {name,username,email,password} = registerBodySchema.parse(request.body)
 
+
+    //Verificação por query , para verificar os Emails e Usernames , pois devem ser unitários
+    const userWithSameEmailOrUsername = await prisma.user.findFirst({
+        where:{
+            OR:[
+                {username},
+                {email}
+            ]  
+        }           
+    })
+
+    if(userWithSameEmailOrUsername){
+        return reply.status(409).send({message: 'Esse Email ou Username já está em uso'})
+    }
+
+    //Criptografia da senha enviada pelo user
+    const passwordHash = await hash(password,env.HASH_SALT_ROUNDS)
+
     const user = await prisma.user.create({
         data: {
             name,
             username,
             email,
-            passwordHash: password
+            passwordHash
         },
 
     })
